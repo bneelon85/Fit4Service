db87aa980902d7273621fce19b92403faf127a3a
Bobby Neelon <bneelon@Bobbys-MBP.localdomain>
2018-08-16 19:01:46 -0500

    more REST

--- Backend/urls.py
+++ Backend/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^account/', include('accounts.urls'))
+    url(r'^account/', include('accounts.urls')),
+    url(r'^api-auth/', include('rest_framework.urls'))
 ]
